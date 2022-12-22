3a28f99d7669df2295fd2549d603c66344a79df4
Tyler <Tyler29475@gmail.com>
2022-12-22 12:07:31 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+
+print("Hello, welcome to my Band Name Generator")
+x = input("Which city did you grow up in?\n  ")
+y = input("What is the name of your favorite pet?\n  ")
+print("Your band name is;\n  " + x, y)
